f2b6e06ef09d1e9339d94f18745f61dac9a05908
unknown <darc.tellier@gmail.com>
2025-04-14 14:59:44 -0400

    modified moving wall code to act more like Thwomp

--- Project/SetPieces/Platform/push_wall.gd
+++ Project/SetPieces/Platform/push_wall.gd
@@ -1,12 +1,48 @@
 extends Node2D
-@export var speed = 1
 
+var player_reff: Node2D = null
+var is_thrusting := false
 
-func _on_interaction_area_body_entered(body: Node2D) -> void:
+@export var thrust_time := 0.8  # Time to smash down
+@export var retract_time := 3.2  # Time to return
+@export var wait_before_retract := 0.3
+
+var thrust_tween: Tween = null
+var retract_tween: Tween = null
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		move_wall()
+		player_reff = body
+		if not is_thrusting:
+			thrust()
 
-func move_wall():
-	var tween = create_tween()
-	tween.tween_property($AnimatableBody2D,"position",$End.position,speed).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	tween.play()
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		player_reff = null
+
+func thrust():
+	is_thrusting = true
+	if retract_tween: retract_tween.kill()
+
+	thrust_tween = create_tween()
+	thrust_tween.tween_property($AnimatableBody2D, "position", $End.position, thrust_time).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	thrust_tween.play()
+
+	# Start retract timer after hitting bottom
+	await thrust_tween.finished
+	await get_tree().create_timer(wait_before_retract).timeout
+	retract()
+
+func retract():
+	if thrust_tween: thrust_tween.kill()
+
+	retract_tween = create_tween()
+	retract_tween.tween_property($AnimatableBody2D, "position", get_parent().position, retract_time).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	retract_tween.play()
+
+	await retract_tween.finished
+	is_thrusting = false
+
+	# If player is still there, repeat
+	if player_reff:
+		thrust()
